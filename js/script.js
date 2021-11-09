b579209ebffb385182c015e2de00db2c8e1df40e
giulio30795 <giulioincarbone95@gmail.com>
2021-11-09 14:02:19 +0100

    check

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Traccia:
+// Data la lista di prodotti in allegato, organizziamo un carrello per un ipotetico shop virtuale.
+
+// Avremo una pagina di prodotti che mostrera’ tutti gli articoli presenti in catalogo; al click di un prodotto, nella sezione sottostante alla lista si visualizzera’ il dettaglio del prodotto selezionato con prezzo, categoria e descrizione (da ora chiameremo questa sezione: “dettaglio prodotto”)
+
+// All’interno del dettaglio prodotto ci saranno due bottoni: il primo aggiunge il prodotto al carrello, il secondo alla lista dei desideri
+
+// Nell’header del nostro e-shop verra’ poi riportato il numero di prodotti aggiunto nel carrello e il numero di prodotti aggiunto nella lista dei desideri
+
+// al click del bottone “Acquista” comparira’ un messaggio di ringraziamento e il carrello si svuotera’
+
+// In questo esercizio la creazione del layout e’ a scelta del team, quindi sbizzarritevi. Ricordate: in questo caso non importa riprodurre il layout di uno store reale.
+
+// Il punto focale dell’esercizio e’ capire che codice possiamo riutilizzare e come raggrupparlo in funzioni. 
+
+console.log('ok');
